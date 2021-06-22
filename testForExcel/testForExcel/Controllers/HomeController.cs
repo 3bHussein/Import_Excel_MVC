761efaf07fca3ca9321e647273ca06728ef5754f
3bHussein <39864308+3bHussein@users.noreply.github.com>
2021-06-22 13:26:05 +0200

    Yes :)

---
+++ testForExcel/testForExcel/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.OleDb;
+using System.IO;
+namespace testForExcel.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+        OleDbConnection Econ;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            string filepath = "/excelfolder/" + filename;
+            file.SaveAs(Path.Combine(Server.MapPath("/excelfolder"), filename));
+            InsertExceldata(filepath, filename);
+
+            return View();
+        }
+
+        private void ExcelConn(string filepath)
+        {
+            string constr = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0 Xml;HDR=YES;""", filepath);
+            Econ = new OleDbConnection(constr);
+
+        }
+
+        private void InsertExceldata(string fileepath, string filename)
+        {
+            string fullpath = Server.MapPath("/excelfolder/") + filename;
+            ExcelConn(fullpath);
+            string query = string.Format("Select * from [{0}]", "Sheet1$");
+            OleDbCommand Ecom = new OleDbCommand(query, Econ);
+            Econ.Open();
+
+            DataSet ds = new DataSet();
+            OleDbDataAdapter oda = new OleDbDataAdapter(query, Econ);
+            Econ.Close();
+            oda.Fill(ds);
+
+            DataTable dt = ds.Tables[0];
+
+            SqlBulkCopy objbulk = new SqlBulkCopy(con);
+            objbulk.DestinationTableName = "tbl_registration";
+            objbulk.ColumnMappings.Add("Email", "Email");
+            objbulk.ColumnMappings.Add("Password", "Password");
+            objbulk.ColumnMappings.Add("Name", "Name");
+            objbulk.ColumnMappings.Add("Address", "Address");
+            objbulk.ColumnMappings.Add("City", "City");
+            con.Open();
+            objbulk.WriteToServer(dt);
+            con.Close();
+        }
+
+    }
+}
---
+++ testForExcel/testForExcel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="testForExcel.MvcApplication" Language="C#" %>
